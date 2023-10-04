837e099d9368db1d767c224e1354678acf8906b7
Max <stadibenker@gmail.com>
2023-10-05 00:57:51 +0300

    TDD - greeen test

--- angular-app/src/codewars/components/server-workload/server-workload.component.ts
+++ angular-app/src/codewars/components/server-workload/server-workload.component.ts
@@ -10,7 +10,16 @@ export class ServerWorkloadComponent {
 	// initNodes(), getDistributedJobs(), addNode(), addJob() - YAGNI
 
 	public distributeJobs(nodesCount: number, jobsCount: number): number[][] {
-		const result = [[]];
+		const result: number[][] = [];
+		let nodeIndex = 0;
+		for (let jobNumber = 0; jobNumber < jobsCount; jobNumber++) {
+			result[nodeIndex] = result[nodeIndex] ?? [];
+			result[nodeIndex].push(jobNumber);
+			nodeIndex++;
+			if (nodeIndex === nodesCount) {
+				nodeIndex = 0;
+			}
+		}
 		return result;
 	};
 }
