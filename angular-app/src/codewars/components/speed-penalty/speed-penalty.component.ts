8496d71b9add887e9102b31d38ee69fb00511725
Max <stadibenker@gmail.com>
2023-10-19 14:00:37 +0300

    penalty - more green tests

--- angular-app/src/codewars/components/speed-penalty/speed-penalty.component.ts
+++ angular-app/src/codewars/components/speed-penalty/speed-penalty.component.ts
@@ -10,23 +10,50 @@ export class SpeedPenaltyComponent {
 		return array.every(x => x === 0);
 	}
 	
-	private _getNumberFromArray(array: number[]): string {
+	private _createWholeNumberStr(array: string[]): string {
 		return array.toString().replaceAll(',', '');
 	}
 
-	public getPenalty(conditionsStrArray: string[]): string {
-		const conditions = conditionsStrArray.map(x => Number(x));
-		if (this._isZeroArray(conditions)) {
-			return '0';
+	/*
+		conditions = conditions without zero
+		while(conditions.length > 0) {
+			bestNumber = this._getBestNumber(conditions);
+			bestNumberArray += bestNumber;
+			conditions -= bestNumber;
 		}
-		conditions.sort((c1, c2) => c1 - c2);
-		const amountOfZero = conditions.lastIndexOf(0) + 1;
-		if (amountOfZero > 0) {
-			const filteredConditions = conditions.filter(x => x !== 0);
-			const firstNumber = filteredConditions.shift();
-			const optimizedPenalty = [firstNumber, ...new Array(amountOfZero).fill(0), ...filteredConditions];
-			return this._getNumberFromArray(optimizedPenalty);
+		bestNumberArray = bestNumberArray[0] + zerozArray + bestNumberArray
+	*/
+
+	private _getBestNumbers(conditionsArray: string[], rank: number): string[] {
+		if (conditionsArray.length === 1 || conditionsArray.every(x => x === conditionsArray[0])) {
+			return conditionsArray;
 		}
-		return this._getNumberFromArray(conditions);
+		let result: string [] = [];
+		conditionsArray.forEach(number => {
+			if (!result[0] && number !== '0') {
+				result = [number];
+			} else if (number[rank] && number[rank] === result[0][rank]) {
+				result.push(number);
+			} else if (number[rank] < result[0][rank]) {
+				result = [number];
+			} else if (!number[rank] && result[0][rank] !== '0') {
+				result = [number];
+			} else if (number[rank] === '0' && !result[0][rank]) {
+				result = [number];
+			} else {
+				return;
+			}
+		});
+		return this._getBestNumbers(result, rank + 1);
+	}
+
+	public getPenalty(conditionsArray: string[]): string {
+		let result: string[] = [];
+		while (conditionsArray.length > 0) {
+			const bestNumbers = this._getBestNumbers(conditionsArray, 0);
+			result = [...result, ...bestNumbers];
+			conditionsArray = conditionsArray.filter(x => x !== bestNumbers[0]);
+		}
+		return this._createWholeNumberStr(result);
 	}
 }
