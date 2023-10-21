52b3b348417665d52bc76c0a96a144a006dd045f
Max <stadibenker@gmail.com>
2023-10-21 17:38:43 +0300

    penalty - best number 3 - red test

--- angular-app/src/codewars/components/speed-penalty/speed-penalty.component.ts
+++ angular-app/src/codewars/components/speed-penalty/speed-penalty.component.ts
@@ -12,48 +12,73 @@ export class SpeedPenaltyComponent {
 		return conditionsArray.every(x => x === this._zero);
 	}
 
-	private _getBestNumber2(conditionsArray: string[]): string {
-		if (conditionsArray.length === 1) {
-			return conditionsArray[0];
-		}
-		let result: number = Number.MAX_VALUE;
-		for(let i = 0; i < conditionsArray.length; i++) {
-			for(let j = 0; j < conditionsArray.length; j++) {
-				if (i >= j) {
-					continue;
-				}
-				const stuckNumber1 = Number(conditionsArray[i] + conditionsArray[j]);
-				const stuckNumber2 = Number(conditionsArray[j] + conditionsArray[i]);
-				const bestNumber = Number(stuckNumber1 < stuckNumber2 ? conditionsArray[i] : conditionsArray[j]);
-				if (bestNumber < result) {
-					result = bestNumber;
-				}
-			}
-		}
-		return result.toString();
+	private _getBestNumber3 (conditionsArray: string[]): string {
+		let bestNumber = conditionsArray[0];
+		conditionsArray.forEach(number => {
+			bestNumber = this._compareInternalRanks(bestNumber, number);
+		});
+		return bestNumber;
 	}
 
-	private _getBestNumbers(conditionsArray: string[], rank: number): string[] {
-		if (conditionsArray.length === 1 || conditionsArray.every(x => x === conditionsArray[0])) {
-			return conditionsArray;
-		}
-		let result: string [] = [];
-		conditionsArray.forEach(number => {
-			if (!result[0]) {
-				result = [number];
-			} else if (number[rank] && number[rank] === result[0][rank] || number === result[0]) {
-				result.push(number);
-			} else if (number[rank] < result[0][rank] || number[rank] < result[0][rank - 1]) {
-				result = [number];
-			} else if (!number[rank] && result[0][rank] !== this._zero && number[rank - 1] < result[0][rank]) {
-				result = [number];
-			} else if (number[rank] === this._zero && !result[0][rank]) {
-				result = [number];
-			} else {
-				return;
+	// private _getBestNumber2(conditionsArray: string[]): string {
+	// 	if (conditionsArray.length === 1) {
+	// 		return conditionsArray[0];
+	// 	}
+	// 	const stuckNumbersArray = [];
+	// 	let result: number = Number.MAX_VALUE;
+	// 	for(let i = 0; i < conditionsArray.length; i++) {
+	// 		for(let j = 0; j < conditionsArray.length; j++) {
+	// 			if (i >= j) {
+	// 				continue;
+	// 			}
+	// 			const stuckNumber1 = Number(conditionsArray[i] + conditionsArray[j]);
+	// 			const stuckNumber2 = Number(conditionsArray[j] + conditionsArray[i]);
+	// 			const bestNumber = Number(stuckNumber1 < stuckNumber2 ? conditionsArray[i] : conditionsArray[j]);
+	// 			if (bestNumber < result) {
+	// 				result = bestNumber;
+	// 			}
+	// 		}
+	// 	}
+	// 	return result.toString();
+	// }
+
+	// private _getBestNumbers(conditionsArray: string[], rank: number): string[] {
+	// 	if (conditionsArray.length === 1 || conditionsArray.every(x => x === conditionsArray[0])) {
+	// 		return conditionsArray;
+	// 	}
+	// 	let result: string [] = [];
+	// 	conditionsArray.forEach(number => {
+	// 		if (!result[0]) {
+	// 			result = [number];
+	// 		} else if (number[rank] && number[rank] === result[0][rank] || number === result[0]) {
+	// 			result.push(number);
+	// 		} else if (number[rank] < result[0][rank] || number[rank] < result[0][rank - 1]) {
+	// 			result = [number];
+	// 		} else if (!number[rank] && result[0][rank] !== this._zero && number[rank - 1] < result[0][rank]) {
+	// 			result = [number];
+	// 		} else if (!result[0][rank] && number[rank]) {
+	// 			result = [this._compareInternalRanks(result[0], number)];
+	// 		} else {
+	// 			return;
+	// 		}
+	// 	});
+	// 	return this._getBestNumbers(result, rank + 1);
+	// }
+
+	private _compareInternalRanks(firstNumber: string, secondNumber: string): string {
+		const lessNumber = firstNumber.length < secondNumber.length ? firstNumber : secondNumber;
+		const biggerNumber = firstNumber.length > secondNumber.length ? firstNumber : secondNumber;
+		let lessRank = 0;
+		for (let i = 0; i < biggerNumber.length; i++) {
+			if(lessNumber[lessRank] !== biggerNumber[i]) {
+				return lessNumber[lessRank] < biggerNumber[i] ? lessNumber : biggerNumber;
 			}
-		});
-		return this._getBestNumbers(result, rank + 1);
+			lessRank++;
+			if (lessRank === lessNumber.length) {
+				lessRank = 0
+			}
+		}
+		return lessNumber;
 	}
 
 	private _createWholeNumberStr(array: string[]): string {
@@ -66,10 +91,10 @@ export class SpeedPenaltyComponent {
 		}
 		let result: string[] = [];
 		while (conditionsArray.length > 0) {
-			const bestNumber = this._getBestNumber2(conditionsArray);
-			result.push(bestNumber);
-			const index = conditionsArray.findIndex(x => x === bestNumber);
-			conditionsArray.splice(index, 1);
+			const bestNumber = this._getBestNumber3(conditionsArray);
+            result.push(bestNumber);
+            const index = conditionsArray.findIndex(x => x === bestNumber);
+            conditionsArray.splice(index, 1);
 		}
 		const nonZeroIndex = result.lastIndexOf(this._zero);
 		if (nonZeroIndex > -1) {
