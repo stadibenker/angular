a135af140feac2357edc4f61eb950bc3e7d5c9a8
Max <stadibenker@gmail.com>
2023-11-05 12:56:01 +0200

    i18n - initial component; red test

---
+++ angular-app/src/codewars/components/i18n/i18n.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { I18nComponent } from './i18n.component';
+
+describe('I18nComponent', () => {
+	let component: I18nComponent;
+	let fixture: ComponentFixture<I18nComponent>;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+		declarations: [I18nComponent]
+		});
+		fixture = TestBed.createComponent(I18nComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	describe('abbreviate', () => {
+		test.each([
+			['internationalization', 'i18n'],
+			['elephant-ride', 'e6t-r2e'],
+		])
+		('when words is %p, it should return abbribiation %p', (words, expectedResult) => {
+			const result = component.abbreviate(words);
+			expect(result).toEqual(expectedResult);
+		});
+	});
+});
---
+++ angular-app/src/codewars/components/i18n/i18n.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-i18n',
+	templateUrl: './i18n.component.html',
+	styleUrls: ['./i18n.component.scss']
+})
+export class I18nComponent {
+	public abbreviate(words: string) {
+		return '';
+	}
+}
